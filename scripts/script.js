de9455b0dfec31875d9b1f6abc0e7508e03da5dd
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2021-05-19 14:11:27 +0200

    added screen responsiveness

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('hamburger');
+const social = document.getElementById('social');
+
+hamburger.addEventListener('click',()=>{
+navUL.classList.toggle('show');
+});
